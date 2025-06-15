3c08ed426fb9289be23c8c2c27eed1d4445012ef
Matias Pacini <matiaspacini052@gmail.com>
2025-06-15 20:44:09 -0300

    Update authRoutes.ts

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -4,10 +4,10 @@ import * as authController from '../controllers/authController';
 
 const router = express.Router();
 
-// Esta ruta debería usar el middleware de autenticación de passport
+router.post('/register', authController.register);
 router.post('/login', passport.authenticate('local'), authController.login);
 router.post('/logout', authController.logout);
 router.get('/me', authController.getCurrentUser);
 
-router.post('/register', authController.register);
+
 export default router;
